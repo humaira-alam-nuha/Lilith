848a28b510b490563ed8e76f667c5103c65e1091
Your Name <nuhahumairaalam@gmail.com>
2025-09-11 12:16:12 +0600

    Adding Startup Sound

---
+++ Engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+#Playing Assistant Sound Function
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\computer-startup-sound-effect-312870.mp3"
+    playsound(music_dir)
